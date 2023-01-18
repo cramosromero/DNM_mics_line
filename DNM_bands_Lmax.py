ff48227a63bb3fc714bffd51f7ed1c9ee0bdef2c
Carlos Ramos Romero <C.A.RamosRomero@salford.ac.uk>
2023-01-18 11:31:36 +0000

    Update DNM_bands_Lmax.py

--- DNM_bands_Lmax.py
+++ DNM_bands_Lmax.py
@@ -11,6 +11,7 @@ References of microphone configuration: "NASA-UNWG/SG2
 UAM Ground & Flight Test Measurement Protocol""
  
 """
+import math
 import os
 import matplotlib.pyplot as plt
 import scienceplots #https://www.reddit.com/r/learnpython/comments/ila9xp/nice_plots_for_scientific_papers_theses_and/
@@ -19,11 +20,13 @@ plt.style.use(['science', 'grid'])
 from cycler import cycler
 line_cycler     = cycler(linestyle = ['-', '--', '-.', (0, (3, 1, 1, 1)), ':'])
 color_cycler    = cycler(color = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5'])
+marker = ['d', '*', '^', 's', 'X', '2']
 line_color_cycler = (len(line_cycler)*color_cycler
                      + len(color_cycler)*line_cycler)
 lc = line_color_cycler()
 # import pandas as pd
 from matplotlib import rc
+
 # from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from matplotlib.cbook import get_sample_data
@@ -52,12 +55,12 @@ to_bands =[20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250,
 
 # FIELD MEASUREMENTS 
 PILOT   = 'Ed'
-DroneID = 'M3' #{M3, 3p, Yn,Fp}
+DroneID = 'Yn' #{M3, 3p, Yn,Fp}
 HAGL    = 10
 OPE     = 'F15' #{F15, F05, F27}
-PYL     = 'N' #{Y, N}
+PYL     = 'Y' #{Y, N}
 STA     = 'W' #{E, W}
-Date    = '??????'
+Date    = '??????' #{hhmmss}
 
 Count   = 'dw' #{uw: upwind, dw:  downwind}
 
@@ -75,7 +78,7 @@ print(identifier)
 fly_speed = float(OPE[-2:])
 
 # ACCCES DATA_ACU SINGLE EVENT
-event   = 3 # {1,2,3,4,5,6,7,8,9... nEve}
+event   = 1 # {1,2,3,4,5,6,7,8,9... nEve}
 acu_metric = 'LAFp' # LAFp, LZfp
 
 Fs, TT, DATA_raw, DATA_acu = FileTools.data_single_events (files[event-1], n_mics, acu_metric)
@@ -95,7 +98,7 @@ plt.ylabel('Pressure [Pa]')
 plt.xlabel('Time [s]')
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'allChan_Press'+'Flyby{}'.format(event)+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+# # plt.show()
 
 # %% DATA_RAW and DATA_ACU ALL events 1-mic
 # ##################################################
@@ -125,7 +128,7 @@ plt.ylabel('Pressure [Pa]')
 plt.xlabel('Time [s]')
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'allFlybys_Press'+'Chan{}'.format(event)+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+# # plt.show()
 
 # %% SIGNAL SEGMENTATION 
 ##############################
@@ -150,8 +153,8 @@ ax0.get_legend().set_title("Flybys")
 plt.ylabel(' Amplitude [Pa]')
 plt.xlabel('Time [s]')
 plt.tight_layout()
-# plt.savefig(ffolder+'/'+identifier+'_'+'allFlybys_Press'+'Flyby{}'.format(event)+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+plt.savefig(ffolder+'/'+identifier+'_'+'allFlybys_Press'+'Flyby{}'.format(event)+'.jpg', format='jpg',dpi=1200)
+# # plt.show()
     
 # %%%% If data selectio depends on threshold 
 if segmented_based == 'time':
@@ -159,7 +162,7 @@ if segmented_based == 'time':
 
 elif segmented_based == 'level': 
     """ Based on time signals lower than the Lmax"""
-    THRESH = 20
+    THRESH = 10
     TIME_r, vec_time, Data_raw_segmented_trh, Data_acu_segmented_trh  = TimeTools.segment_THRESH(THRESH,Fs,Data_raw_segmented, Data_acu_segmented)
        
     ## PLOT segmented by threshold
@@ -172,7 +175,7 @@ elif segmented_based == 'level':
     ax0.get_legend().set_title("Events")
     plt.ylabel(' Amplitude [Pa]')
     plt.xlabel('Time [s]')
-    plt.show()
+    # plt.show()
     
     Data_raw_segmented = Data_raw_segmented_trh
     Data_acu_segmented = Data_acu_segmented_trh
@@ -213,7 +216,7 @@ fig.supylabel('Amplitude [dB/Hz] re $(20 uPa)^2$')
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'Flybys_PSD'+'Mic{}'.format(microphone)+'.jpg', format='jpg',dpi=1200)
 
-# plt.show()
+# #plt.show()
 
 fig, (ax0) = plt.subplots(1, figsize=(6.7,3.8))
 
@@ -235,7 +238,7 @@ ax0.set_xlabel('Frequency [Hz]')
 ax0.set_title('Microphone {}'.format(microphone)) 
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'Flybys_PSD_all'+'Mic{}'.format(microphone)+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+# #plt.show()
 
 #
 """PSD same microphone, all Events MEAN and STD"""
@@ -264,25 +267,31 @@ plt.tight_layout()
 plt.legend()
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'Mean_PSD_all'+'Mic{}'.format(microphone)+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+# # plt.show()
 
 # %%Spectrogram
 # ################################
 """PLOT-SPECTROGRAM"""  
 import scipy.signal as ss 
 #eve_mic = [int, int]
-eve_mic = [1, 5]
+eve_mic = [1, 5]#{1,2,3,4,5,6,7,8,9}
 
-x = DATA_raw_events[eve_mic[0]-1,:,eve_mic[1]-1]
+ti=2*Fs
+tf=22*Fs
+
+# x = DATA_raw_events[eve_mic[0]-1,:,eve_mic[1]-1]
+x = DATA_raw_events[eve_mic[0]-1,ti:tf,eve_mic[1]-1]
 
 fig, (ax0) = plt.subplots(figsize=(6,3))
-f, t, Sxx = ss.spectrogram(x, nperseg=(2**18)//8, fs=Fs, nfft=2**18)
+f, t, Sxx = ss.spectrogram(x, nperseg=(2**16)//6, fs=Fs, nfft=2**18)
 
-plt.pcolormesh(t, f, 10*np.log10(Sxx/(20e-6)**2), cmap='jet', vmin=5,vmax=60, shading='auto')
-plt.title('Spectrogram of Flyby {}'.format(eve_mic[0]))
+y_lim =[100,400]
+#RdBu_r
+plt.pcolormesh(t, f, 10*np.log10(Sxx/(20e-6)**2),vmin=10,vmax=60,cmap='viridis',shading='auto') # vmin=10,vmax=60 
+
 plt.xlabel("Time [s]")
 plt.ylabel("Frequency [Hz]")
-plt.ylim(100,500)
+plt.ylim(y_lim[0],y_lim[1])
 cbar = plt.colorbar()
 cbar.set_label("SPL [dB/Hz]")
 plt.tight_layout()
@@ -294,19 +303,23 @@ spectrum = plt.specgram(x, NFFT=2**17, Fs=Fs,cmap='jet_r') #, noverlap=2**5-1
 plt.title('Spectrogram of Flyby {}'.format(eve_mic[0]))
 plt.xlabel("Time [s]")
 plt.ylabel("Frequency [Hz]")
-plt.ylim(100,500)
+plt.ylim(y_lim[0],y_lim[1])
 cbar = plt.colorbar()
 cbar.set_label("Power/frequency [dB/Hz]")
 plt.tight_layout()
 plt.savefig(ffolder+'/'+identifier+'_'+'SpectB'+'.jpg', format='jpg',dpi=1200)
-# plt.show()
+plt.show()
 
-
+# %%% Storing DI
+DI_SPL_lev= []
+DI_SPL_std= []
+Band_names= []
 #%% BAND Selection for Directivity
 ####################################
-
 """ Selecciion de SPL ALL EVENTS on EACH MICROPHONE"""
-band_selection='oto_band' #{'we_band', 'oto_band'}
+SPL_events_mic_bands = []
+
+band_selection='oto_band' #{'my_band', 'oto_band'}
 
 # %%%% whichever band
 #####################
@@ -314,11 +327,11 @@ band_selection='oto_band' #{'we_band', 'oto_band'}
 # freqs_selected = line1._x # from plot selection if is avaliable
 # A = freq >= freqs_selected[0]
 # B = freq <= freqs_selected[-1]
-if band_selection == 'we_band':
+fd_ = 100
+fu_ = 200
 
-    fd_ = 100
-    fu_ = 400
-    band = (fu_+fd_)//2
+if band_selection == 'my_band':
+    Band = [(fu_+fd_)//2]
     
     A = freq >= fd_
     B = freq <= fu_
@@ -329,7 +342,8 @@ if band_selection == 'we_band':
     SPL_event_mic = 10*np.log10(p2_sum_selected/(p_ref**2)) 
     
     SPL_event_mic = SPL_event_mic.reshape(DATA_raw_events.shape[0],1, DATA_raw_events.shape[2]) #[eve,SPL,channel]
-    label = '$SPL[dB]$ at band {} Hz'.format(band)
+    SPL_events_mic_bands.append(SPL_event_mic) #save in the list for depropagation
+    band_name = "BPF narrow band: "+str(fd_)+" to "+str(fu_)
 # %%%% among 1/3octave band or overall level
 #####################
 elif band_selection == 'oto_band':
@@ -338,62 +352,81 @@ elif band_selection == 'oto_band':
     fraction = 3
     DATA_SPLoverall, DATA_SPLs, freq = FreqTools.calc_SPLs(Data_raw_segmented,Fs, fraction, limits=[20, 20000], show=0)
     
-    band = 200
-    
     """ [20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250,
             315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000,
             5000, 6300, 8000, 10000, 12500, 16000, 20000, 'overall'] """
     
-    if type(band) == int:
-        b_index = to_bands.index(band)
-        
+    Band = [50, 63, 80, 100, 125, 160, 200, 250,
+            315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000,
+            5000, 6300, 8000, 10000]
+    
+    for bb in range(len(Band)):
+        b_index = to_bands.index(Band[bb])
         spl_s_mean = np.mean(DATA_SPLs[:,:,:],axis=0)
         spl_s_std = np.std(DATA_SPLs[:,:,:],axis=0)
-        
         SPL_event_mic = DATA_SPLs[:,b_index,:] # data [events]
         SPL_event_mic = SPL_event_mic.reshape(DATA_raw_events.shape[0],1, DATA_raw_events.shape[2]) #[eve,SPL,channel]
-        label = '$SPL[dB]$ at band {} Hz'.format(band)
+        SPL_events_mic_bands.append(SPL_event_mic) #save in the list for depropagation
     
-    elif (type(band) == str and band =='overall') == True: ## to calculate the overall SPL from bands
-        b_index = to_bands.index(1000)
-        band = 1000 #for atmospheric correction
+    if len(Band)==1:
+        band_name = str(Band[0])
+    else:
+        band_name = "OASP"
         
-        spl_s_mean = np.mean(DATA_SPLoverall[:,:,:],axis=0)
-        spl_s_std = np.std(DATA_SPLoverall[:,:,:],axis=0)
-        
-        SPL_event_mic = DATA_SPLoverall[:,:,:] # data [events]
-        label = '$SPL[dB]$ Overall'
-        
-
 # %% DEPROPAGATION
 """ SPL DEPROPAGATION"""
 
 rad_to_deprop = 1 #[m]
 heightAGL = HAGL #[m]
 
-Lmax_array = SPL_event_mic
+# Lmax_array = SPL_events_mic_bands
 
-L_array_depropagated, thetas, dist_ground_mics, phi_dx = AircraftTools.depropagator(Lmax_array, heightAGL, rad_to_deprop, band, dx=0)
+results_depro = []
+for arr in range(len(SPL_events_mic_bands)):
+    Lmax_array = SPL_events_mic_bands[arr]
+    band = Band[arr]
+    L_array_depropagated, thetas, dist_ground_mics, phi_dx = AircraftTools.depropagator(Lmax_array, heightAGL, rad_to_deprop, band, dx=0)
+    results_depro.append(L_array_depropagated)
 
 theta_ref = np.max(np.array(thetas))
 theta_ref_radians = (theta_ref/180)*np.pi
 
-L_array_deprop_mean = np.squeeze(np.mean(L_array_depropagated, axis=0))
-L_array_deprop_std = np.squeeze(np.std(L_array_depropagated, axis=0))
+L_array_depropagated = results_depro
 
-DI_events = AircraftTools.directivity (L_array_depropagated)
+All_L_array_deprop_mean = []
+All_L_array_deprop_std = []
+for arr in range(len(results_depro)):
+    L_array_deprop_mean = np.squeeze(np.mean(L_array_depropagated[arr], axis=0))
+    L_array_deprop_std = np.squeeze(np.std(L_array_depropagated[arr], axis=0))
+    All_L_array_deprop_mean.append(L_array_deprop_mean)
+    All_L_array_deprop_std.append(L_array_deprop_std)
+      
+np.array(All_L_array_deprop_std)  
+"""Levels After Depropagation pack MEAN and STD"""
+L_array_deprop_mean = np.array(All_L_array_deprop_mean)
+L_array_deprop_mean = sum(10**(L_array_deprop_mean/10),1)
+L_array_deprop_mean = [10*math.log10(i) for i in L_array_deprop_mean] # Logaritmic summ
 
-DI_mean = np.squeeze(np.mean(DI_events, axis=0))
-DI_std = np.squeeze(np.std(DI_events, axis=0))
-
-"""Levels After Depropagation"""
+L_array_deprop_std = np.array(All_L_array_deprop_std)
+L_array_deprop_std = np.mean(L_array_deprop_std, axis=0) # Logaritmic summ
+"""Levels After Depropagation MEAN and STD"""
 LEVEL_TO_PLOT = L_array_deprop_mean
 STD_TO_PLOT = L_array_deprop_std
+
+if Count =='dw' : ##flip the aray if the drone flyes up-wind (for the order in the Sphere)
+    LEVEL_TO_PLOT = np.flip(LEVEL_TO_PLOT, 0)
+    STD_TO_PLOT = np.flip(STD_TO_PLOT, 0)
     
+#%%%% All curves
+#########################
+"""ALl curves' plots"""
+DI_SPL_lev.append(LEVEL_TO_PLOT)
+DI_SPL_std.append(STD_TO_PLOT)
+Band_names.append(band_name)
 #%%%% PLOTS DIRECTIVITY-LEVEL mean std, all events - all mics
 #########################
 """PLOTS DIRECTIVITY-LEVEL mean std, all events - all mics"""
-band = str(band)
+
 dfor = next(lc) #draw_format
 dfor = dfor["color"] + dfor["linestyle"]
 
@@ -406,17 +439,21 @@ theta_ref_radians = (theta_ref/180)*np.pi
 
 theta = np.linspace((np.pi/2)-theta_ref_radians, (np.pi/2)+theta_ref_radians, len(thetas))
 
-fig = plt.figure(figsize=(3.9,3.3))
+fig = plt.figure(figsize=(6.23, 5.24))
 ax = fig.add_subplot(111, polar=True)
-ax.plot(theta, LEVEL_TO_PLOT, dfor, label=label)
+
+for pp in range(len(DI_SPL_lev)):
+    form=next(lc)
+    ax.plot(theta, np.array(DI_SPL_lev[pp]), dfor, label=Band_names[pp]+" Hz",
+        color=form["color"],marker=marker[pp],linestyle=form['linestyle'],linewidth=1)
+    plt.fill_between(theta, DI_SPL_lev[pp]-DI_SPL_std[pp],
+                  DI_SPL_lev[pp]+DI_SPL_std[pp],
+                  color=form["color"], alpha=0.1)
+
 ax.legend(loc='lower center')
 
-plt.fill_between(theta, LEVEL_TO_PLOT - L_array_deprop_std,
-                  LEVEL_TO_PLOT + L_array_deprop_std,
-                  color='gray', alpha=0.3)
-
-r_min = TimeTools.round_up(np.min(LEVEL_TO_PLOT),10) - 15
-r_max = TimeTools.round_up(np.max(LEVEL_TO_PLOT),10)
+r_min = TimeTools.round_up(np.min(DI_SPL_lev),10) - 15
+r_max = TimeTools.round_up(np.max(DI_SPL_lev),5)
 r_r = r_min-5
 
 ax.set_rmin(r_min)
@@ -432,73 +469,47 @@ ax.set_xticks(((np.arange(len(thetas_t))*15)+30)*np.pi/180)
 
 ax.set_xticklabels(thetas_t)
 
-ax.set_title('Directivity pattern'+'\n'+
-              'Drone: ' + DroneID +'\n'+ acu_metric +
-              ' $r=' + str(rad_to_deprop)+'[m]$')
+# ax.set_title('Directivity pattern'+'\n'+
+#               'sUAS: ' + DroneID +'\n'+ acu_metric +
+#               ' $r=' + str(rad_to_deprop)+'[m]$')
+# ax.set_title('sUAS: ' + DroneID)
 plt.tight_layout()
 
 """ Add drone imagen """
 im = imread(get_sample_data(im_drone_file, asfileobj=False))
 oi = OffsetImage(im, zoom=0.15)
 ab = AnnotationBbox(oi, xy=(np.pi*90/180, r_min), frameon=False, clip_on=False, xybox=(90,r_r))#, box_alignment=(1,1))
+ax.annotate('$SPL$ [dB]',
+            xy=(np.pi*90/180, r_min),  # theta, radius
+            xytext=(0.18, 0.7),    # fraction, fraction
+            textcoords='figure fraction',
+            horizontalalignment='left',
+            verticalalignment='bottom',
+            )
+ax.annotate('$\Theta$ [deg]',
+            xy=(np.pi*90/180, r_min),  # theta, radius
+            xytext=(0.48, 0.17),    # fraction, fraction
+            textcoords='figure fraction',
+            horizontalalignment='left',
+            verticalalignment='bottom',
+            )
 plt.gca().add_artist(ab)
 
 ax.set_theta_offset(np.pi) #invert the plot in vertical axis
 ax.yaxis.grid(which='minor', color='#ECECEC', linestyle='--', linewidth=0.8)
-plt.minorticks_on()
-plt.savefig(ffolder+'/'+identifier+'_'+'Dir'+label+'.jpg', format='jpg',dpi=1200)
+# plt.minorticks_on()
+plt.savefig(ffolder+'/'+identifier+'_'+'Dir.jpg', format='jpg',dpi=1200)
 
 # plt.show()
 
-#%%%% PLOTS DIRECTIVITY-INDEX mean std, all events - all mics
-#############################
-"""PLOTS DIRECTIVITY-INDEX mean std, all events - all mics"""
-
-fig = plt.figure(figsize=(3.9,3.3))
-
-ax = fig.add_subplot(111, polar=True)
-ax.plot(theta, DI_mean, dfor, label=label)
-ax.legend(loc='lower center')
-
-plt.fill_between(theta, DI_mean - DI_std,
-                  DI_mean + DI_std,
-                  color='gray', alpha=0.2)
-
-r_min = TimeTools.round_up(np.min(DI_mean),3) - 6
-r_max = TimeTools.round_up(np.max(DI_mean),3)
-r_r = r_min-3
-
-ax.set_rmin(r_min)
-ax.set_rmax(r_max)
-ax.set_rorigin(r_r)
-
-ax.set_rgrids(np.linspace(r_min,r_max,int((r_max-r_min)/3+1)))
-
-ax.set_thetamin(30)
-ax.set_thetamax(150)
-thetas_t = [str(angle) + '\N{DEGREE SIGN}' for angle in thetas] #thetas labels
-ax.set_xticks(((np.arange(len(thetas_t))*15)+30)*np.pi/180)
-
-ax.set_xticklabels(thetas_t)
-
-ax.set_title('DI'+'\n'+
-              'Drone: '+ DroneID +'\n'+
-              ' $r=' + str(rad_to_deprop)+'[m]$')
-plt.tight_layout()
-# ax.minorticks_on()
-""" Add drone imagen """
-im = imread(get_sample_data(im_drone_file, asfileobj=False))
-oi = OffsetImage(im, zoom=0.15)
-ab = AnnotationBbox(oi, xy=(np.pi*90/180, r_min),frameon=False, clip_on=False,xybox=(90,r_r))#, box_alignment=(1,1))
-plt.gca().add_artist(ab)
-
-ax.set_theta_offset(np.pi) #invert the plot in vertical axis
-ax.yaxis.grid(which='minor', color='#ECECEC', linestyle='--', linewidth=0.8)
-plt.minorticks_on()
-plt.savefig(ffolder+'/'+identifier+'_'+'DI'+label+'.jpg', format='jpg',dpi=1200)
-# plt.show()
-#####################    
-#%%RESULTS
+#%%Dictionary for configuration figures
 ######################
-print('results')
-print(identifier)
+C_G_JAST={"identifier"  :identifier,
+          "time_slice"  :time_slice,
+          "microphone"  :microphone,
+          "eve_mic"     :eve_mic,
+          "band"        :Band_names,
+          "band_level"  :DI_SPL_lev,}
+import pickle #https://wiki.python.org/moin/UsingPickle
+#save dictionary to pickle file
+pickle.dump( C_G_JAST, open(ffolder+'/'+identifier+"_Lmax.p", "wb" ) )
